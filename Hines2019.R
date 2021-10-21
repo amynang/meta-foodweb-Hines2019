d31dfe98f479df4f0c2f1a9329d719024c3f9216
Angelos Amyntas <angelos.amyntas@idiv.de>
2021-10-21 21:37:38 +0200

    trying different interaction types

--- Hines2019.R
+++ Hines2019.R
@@ -66,13 +66,204 @@ for (i in 1:16) { #turning get_interaction_matrix() output into proper adjacency
 }
 
 #bring everything together
-f.mat = rbind(cbind(mat.list[[1]],mat.list[[5]],mat.list[[6]],mat.list[[7]]),
+eats = rbind(cbind(mat.list[[1]],mat.list[[5]],mat.list[[6]],mat.list[[7]]),
               cbind(mat.list[[2]],mat.list[[8]],mat.list[[9]],mat.list[[10]]),
               cbind(mat.list[[3]],mat.list[[11]],mat.list[[12]],mat.list[[13]]),
               cbind(mat.list[[4]],mat.list[[14]],mat.list[[15]],mat.list[[16]]))
 
 #turn it into a sparse matrix
-f.mat.sparse = as(f.mat, "sparseMatrix")
-save(f.mat.sparse, file="f.mat.sparse.Rdata")
+eats.sparse = as(eats, "sparseMatrix")
+save(eats.sparse, file="eats.sparse.Rdata")
 
-load("f.mat.sparse.Rdata")
+#load("eats.sparse.Rdata")
+
+
+
+
+mmat.1 = get_interaction_matrix(taxa[1:177], 
+                               taxa[1:177],"preysOn")
+mmat.5 = get_interaction_matrix(taxa[1:177], 
+                               taxa[178:354],"preysOn")
+mmat.6 = get_interaction_matrix(taxa[1:177], 
+                               taxa[355:531],"preysOn")
+mmat.7 = get_interaction_matrix(taxa[1:177], 
+                               taxa[532:705],"preysOn")
+
+mmat.2 = get_interaction_matrix(taxa[178:354], 
+                               taxa[178:354],"preysOn")
+mmat.8 = get_interaction_matrix(taxa[178:354], 
+                               taxa[1:177],"preysOn")
+mmat.9 = get_interaction_matrix(taxa[178:354], 
+                               taxa[355:531],"preysOn")
+mmat.10 = get_interaction_matrix(taxa[178:354], 
+                                taxa[532:705],"preysOn")
+
+mmat.3 = get_interaction_matrix(taxa[355:531], 
+                               taxa[355:531],"preysOn")
+mmat.11 = get_interaction_matrix(taxa[355:531], 
+                                taxa[1:177],"preysOn")
+mmat.12 = get_interaction_matrix(taxa[355:531], 
+                                taxa[178:354],"preysOn")
+mmat.13 = get_interaction_matrix(taxa[355:531], 
+                                taxa[532:705],"preysOn")
+
+mmat.4 = get_interaction_matrix(taxa[532:705], 
+                               taxa[532:705],"preysOn")
+mmat.14 = get_interaction_matrix(taxa[532:705], 
+                                taxa[1:177],"preysOn")
+mmat.15 = get_interaction_matrix(taxa[532:705], 
+                                taxa[178:354],"preysOn")
+mmat.16 = get_interaction_matrix(taxa[532:705], 
+                                taxa[355:531],"preysOn")
+
+
+mmat.list = vector("list",16)
+mmat.list = list(mmat.1, mmat.2, mmat.3, mmat.4, mmat.5,
+                mmat.6, mmat.7, mmat.8, mmat.9, mmat.10,
+                mmat.11,mmat.12,mmat.13,mmat.14,mmat.15,
+                mmat.16)
+for (i in 1:16) { #turning get_interaction_mmatrix() output into proper adjacency mmatrices
+  rownames(mmat.list[[i]]) = mmat.list[[i]][,1]
+  mmat.list[[i]] = as.matrix(mmat.list[[i]][,-1])
+  #colnames(mmat.list[[i]]) = rownames(mmat.list[[i]])
+}
+
+#bring everything together
+preysOn = rbind(cbind(mmat.list[[1]],mmat.list[[5]],mmat.list[[6]],mmat.list[[7]]),
+              cbind(mmat.list[[2]],mmat.list[[8]],mmat.list[[9]],mmat.list[[10]]),
+              cbind(mmat.list[[3]],mmat.list[[11]],mmat.list[[12]],mmat.list[[13]]),
+              cbind(mmat.list[[4]],mmat.list[[14]],mmat.list[[15]],mmat.list[[16]]))
+
+
+preysOn.sparse = as(preysOn, "sparseMatrix")
+save(preysOn.sparse, file="preysOn.sparse.Rdata")
+
+#load("preysOn.sparse.Rdata")
+
+
+
+mmmat.1 = get_interaction_matrix(taxa[1:177], 
+                                taxa[1:177],"preyedUponBy")
+mmmat.5 = get_interaction_matrix(taxa[1:177], 
+                                taxa[178:354],"preyedUponBy")
+mmmat.6 = get_interaction_matrix(taxa[1:177], 
+                                taxa[355:531],"preyedUponBy")
+mmmat.7 = get_interaction_matrix(taxa[1:177], 
+                                taxa[532:705],"preyedUponBy")
+
+mmmat.2 = get_interaction_matrix(taxa[178:354], 
+                                taxa[178:354],"preyedUponBy")
+mmmat.8 = get_interaction_matrix(taxa[178:354], 
+                                taxa[1:177],"preyedUponBy")
+mmmat.9 = get_interaction_matrix(taxa[178:354], 
+                                taxa[355:531],"preyedUponBy")
+mmmat.10 = get_interaction_matrix(taxa[178:354], 
+                                 taxa[532:705],"preyedUponBy")
+
+mmmat.3 = get_interaction_matrix(taxa[355:531], 
+                                taxa[355:531],"preyedUponBy")
+mmmat.11 = get_interaction_matrix(taxa[355:531], 
+                                 taxa[1:177],"preyedUponBy")
+mmmat.12 = get_interaction_matrix(taxa[355:531], 
+                                 taxa[178:354],"preyedUponBy")
+mmmat.13 = get_interaction_matrix(taxa[355:531], 
+                                 taxa[532:705],"preyedUponBy")
+
+mmmat.4 = get_interaction_matrix(taxa[532:705], 
+                                taxa[532:705],"preyedUponBy")
+mmmat.14 = get_interaction_matrix(taxa[532:705], 
+                                 taxa[1:177],"preyedUponBy")
+mmmat.15 = get_interaction_matrix(taxa[532:705], 
+                                 taxa[178:354],"preyedUponBy")
+mmmat.16 = get_interaction_matrix(taxa[532:705], 
+                                 taxa[355:531],"preyedUponBy")
+
+
+mmmat.list = vector("list",16)
+mmmat.list = list(mmmat.1, mmmat.2, mmmat.3, mmmat.4, mmmat.5,
+                 mmmat.6, mmmat.7, mmmat.8, mmmat.9, mmmat.10,
+                 mmmat.11,mmmat.12,mmmat.13,mmmat.14,mmmat.15,
+                 mmmat.16)
+for (i in 1:16) { #turning get_interaction_mmmatrix() output into proper adjacency mmmatrices
+  rownames(mmmat.list[[i]]) = mmmat.list[[i]][,1]
+  mmmat.list[[i]] = as.matrix(mmmat.list[[i]][,-1])
+  #colnames(mmmat.list[[i]]) = rownames(mmmat.list[[i]])
+}
+
+#bring everything together
+preyedUponBy = rbind(cbind(mmmat.list[[1]],mmmat.list[[5]],mmmat.list[[6]],mmmat.list[[7]]),
+               cbind(mmmat.list[[2]],mmmat.list[[8]],mmmat.list[[9]],mmmat.list[[10]]),
+               cbind(mmmat.list[[3]],mmmat.list[[11]],mmmat.list[[12]],mmmat.list[[13]]),
+               cbind(mmmat.list[[4]],mmmat.list[[14]],mmmat.list[[15]],mmmat.list[[16]]))
+
+preyedUponBy.sparse = as(preyedUponBy, "sparseMatrix")
+save(preyedUponBy.sparse, file="preyedUponBy.sparse.Rdata")
+
+
+
+
+
+mmmmat.1 = get_interaction_matrix(taxa[1:177], 
+                                 taxa[1:177],"eatenBy")
+mmmmat.5 = get_interaction_matrix(taxa[1:177], 
+                                 taxa[178:354],"eatenBy")
+mmmmat.6 = get_interaction_matrix(taxa[1:177], 
+                                 taxa[355:531],"eatenBy")
+mmmmat.7 = get_interaction_matrix(taxa[1:177], 
+                                 taxa[532:705],"eatenBy")
+
+mmmmat.2 = get_interaction_matrix(taxa[178:354], 
+                                 taxa[178:354],"eatenBy")
+mmmmat.8 = get_interaction_matrix(taxa[178:354], 
+                                 taxa[1:177],"eatenBy")
+mmmmat.9 = get_interaction_matrix(taxa[178:354], 
+                                 taxa[355:531],"eatenBy")
+mmmmat.10 = get_interaction_matrix(taxa[178:354], 
+                                  taxa[532:705],"eatenBy")
+
+mmmmat.3 = get_interaction_matrix(taxa[355:531], 
+                                 taxa[355:531],"eatenBy")
+mmmmat.11 = get_interaction_matrix(taxa[355:531], 
+                                  taxa[1:177],"eatenBy")
+mmmmat.12 = get_interaction_matrix(taxa[355:531], 
+                                  taxa[178:354],"eatenBy")
+mmmmat.13 = get_interaction_matrix(taxa[355:531], 
+                                  taxa[532:705],"eatenBy")
+
+mmmmat.4 = get_interaction_matrix(taxa[532:705], 
+                                 taxa[532:705],"eatenBy")
+mmmmat.14 = get_interaction_matrix(taxa[532:705], 
+                                  taxa[1:177],"eatenBy")
+mmmmat.15 = get_interaction_matrix(taxa[532:705], 
+                                  taxa[178:354],"eatenBy")
+mmmmat.16 = get_interaction_matrix(taxa[532:705], 
+                                  taxa[355:531],"eatenBy")
+
+
+mmmmat.list = vector("list",16)
+mmmmat.list = list(mmmmat.1, mmmmat.2, mmmmat.3, mmmmat.4, mmmmat.5,
+                  mmmmat.6, mmmmat.7, mmmmat.8, mmmmat.9, mmmmat.10,
+                  mmmmat.11,mmmmat.12,mmmmat.13,mmmmat.14,mmmmat.15,
+                  mmmmat.16)
+for (i in 1:16) { #turning get_interaction_mmmatrix() output into proper adjacency mmmatrices
+  rownames(mmmmat.list[[i]]) = mmmmat.list[[i]][,1]
+  mmmmat.list[[i]] = as.matrix(mmmmat.list[[i]][,-1])
+  #colnames(mmmat.list[[i]]) = rownames(mmmat.list[[i]])
+}
+
+#bring everything together
+eatenBy = rbind(cbind(mmmmat.list[[1]],mmmmat.list[[5]],mmmmat.list[[6]],mmmmat.list[[7]]),
+                     cbind(mmmmat.list[[2]],mmmmat.list[[8]],mmmmat.list[[9]],mmmmat.list[[10]]),
+                     cbind(mmmmat.list[[3]],mmmmat.list[[11]],mmmmat.list[[12]],mmmmat.list[[13]]),
+                     cbind(mmmmat.list[[4]],mmmmat.list[[14]],mmmmat.list[[15]],mmmmat.list[[16]]))
+
+eatenBy.sparse = as(eatenBy, "sparseMatrix")
+save(eatenBy.sparse, file="eatenBy.sparse.Rdata")
+
+
+
+
+
+length(which(f.mat>0))
+length(which(f.mmat>0))
+length(which(f.mmmat>0))
